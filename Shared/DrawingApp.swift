b224345a810a2972763d1433cf37e7c83cee1c3c
Erik Mikac <mikacew@gmail.com>
2021-04-05 08:04:20 -0400

    Initial Commit

---
+++ Shared/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
