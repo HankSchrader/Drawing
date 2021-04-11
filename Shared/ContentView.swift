072853ef59d6b6c04595eace9d81a5ef6b4a5423
Erik Mikac <mikacew@gmail.com>
2021-04-11 08:47:27 -0400

    Continuing drawing module

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,16 +7,50 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var colorCycle = 0.0
-    var body: some View {
-        VStack {
-            ColorCylingCircle(amount: self.colorCycle)
-                .frame(width: 300, height: 300)
+struct Checkerboard: Shape {
+
+    var rows: Int
+    var columns: Int
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / CGFloat(rows)
+        
+        let columnSize = rect.width / CGFloat(columns)
+        
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+
+            // figure out how big each row/column needs to be
+            let rowSize = rect.height / CGFloat(rows)
+            let columnSize = rect.width / CGFloat(columns)
+
+            // loop over all rows and columns, making alternating squares colored
+            for row in 0..<rows {
+                for column in 0..<columns {
+                    if (row + column).isMultiple(of: 2) {
+                        // this square should be colored; add a rectangle here
+                        let startX = columnSize * CGFloat(column)
+                        let startY = rowSize * CGFloat(row)
+
+                        let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                        path.addRect(rect)
+                    }
+                }
+            }
+
+            return path
         }
-        .drawingGroup()
-      
-     }}
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var insetAmount: CGFloat = 50
+    var body: some View {
+       Text("Hello World")
+    }
 
 struct ContentView_Previews: PreviewProvider {
     
